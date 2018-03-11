5c9442653411edb25d651b09ac414b94f5d73e59
marcin_ch <bukkosky@gmail.com>
2018-03-11 12:20:09 +0100

    initial commit

---
+++ src/MenuComponents/PopularMenu/PopularMenu.components.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+
+class PopularMenu extends PureComponent {
+    render() {
+        return(
+            <div className="Menu-components">
+                <h2>Popular Movies</h2>
+            </div>
+        )
+    }
+}
+
+export default PopularMenu;
---
+++ src/MenuComponents/WorstMenu/WorstMenu.component.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+
+
+class WorstMenu extends PureComponent {
+    render() {
+        return(
+            <div className="Menu-components">
+                <h2>Worst Movies</h2>
+            </div>
+        )
+    }
+}
+
+export default WorstMenu;
