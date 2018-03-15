3a0a847d6cf4f2c128c93614f4945edffc033e40
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-15 14:34:49 +0100

    Cleaned components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './index.css';
 import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <Home/>,
+    <BrowserRouter>
+        <div>
+            <Route exact path='/' component={Home}/>
+            {/*<Route path='/' component={Home}/>*/}
+            {/*<Route path='/' component={Home}/>*/}
+            {/*<Route path='/' component={Home}/>*/}
+            {/*<Route path='/' component={Home}/>*/}
+            {/*<Route path='/' component={Home}/>*/}
+            {/*<Route path='/' component={Home}/>*/}
+        </div>
+    </BrowserRouter>,
     document.getElementById('root'));
 registerServiceWorker();
