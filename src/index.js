d957d64b1234434478e6b0740bf5e2bf2acd8904
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-15 14:14:28 +0100

    No commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './Home';
+import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Home/>,
+    document.getElementById('root'));
 registerServiceWorker();
