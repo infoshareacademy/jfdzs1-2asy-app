549a43d3139914c3f4403beea196622049f4bde1
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-16 12:45:40 +0100

    Fixed navigation

---
+++ src/Components/Menu/Menu.component.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import MenuItem from 'material-ui/MenuItem';
+
+class Menu extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <MenuItem>Menu Item</MenuItem>
+                <MenuItem>Menu Item 2</MenuItem>
+            </div>
+        );
+    }
+}
+
+export default Menu;
--- src/Components/Navigation/Navigation.component.js
+++ src/Components/Navigation/Navigation.component.js
@@ -1,14 +1,27 @@
 import React, {PureComponent} from 'react';
-// import {Link} from 'react-router-dom;';
 import AppBar from 'material-ui/AppBar';
+import Drawer from 'material-ui/Drawer';
+import Menu from "../Menu/Menu.component";
 
 class Navigation extends PureComponent {
+
+    constructor(props) {
+        super(props);
+        this.state = {open: false};
+    }
+
+    handleToggle = () => this.setState({open: !this.state.open});
+
     render() {
         return (
             <AppBar
                 title="GitFilm"
+                children={<Drawer open={this.state.open}><Menu/></Drawer>}
+                onLeftIconButtonClick={this.handleToggle}
+                onRightIconButtonClick={this.handleToggle}
                 iconClassNameRight="muidocs-icon-navigation-expand-more"
             />
+
         )
     }
 }
