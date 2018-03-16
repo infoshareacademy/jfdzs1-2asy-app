0954779ba079810ce7a870552265d807ed2a8d48
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-16 14:10:39 +0100

    Added routes do Home.js

--- src/Home.js
+++ src/Home.js
@@ -2,18 +2,26 @@ import React, {PureComponent} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import Navigation from './Components/Navigation/Navigation.component';
+import SearchMovies from "./Routes/SearchMovies/SearchMovies.component";
+import WorstMovies from "./Routes/WorstMovies/WorstMovies.component";
+import FavoriteMovies from "./Routes/FavoriteMovies/FavoriteMovies.component";
+import BestMovies from "./Routes/BestMovies/BestMovies.components";
 import './Home.css';
+import {BrowserRouter, Route} from "react-router-dom";
 
 class Home extends PureComponent {
     render() {
         return (
             <MuiThemeProvider muiTheme={getMuiTheme()}>
-                <div className="Home">
-                    <Navigation/>
-                    <header className="Home-header">
-                        <h1 className="Home-title">Dashboard</h1>
-                    </header>
-                </div>
+                <BrowserRouter>
+                    <div className="Home">
+                        <Navigation/>
+                        <Route path='/favorite' component={FavoriteMovies}/>
+                        <Route path='/search' component={SearchMovies}/>
+                        <Route path='/best' component={BestMovies}/>
+                        <Route path='/worst' component={WorstMovies}/>
+                    </div>
+                </BrowserRouter>
             </MuiThemeProvider>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom'
 import './index.css';
 import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
-import FavoriteMovies from "./Routes/FavoriteMovies/FavoriteMovies.component";
-import SearchMovies from "./Routes/SearchMovies/SearchMovies.component";
-import BestMovies from "./Routes/BestMovies/BestMovies.components";
-import WorstMovies from "./Routes/WorstMovies/WorstMovies.component";
 
-ReactDOM.render(
-    <BrowserRouter>
-        <div>
-            <Route exact path='/' component={Home}/>
-            <Route path='/favorite' component={FavoriteMovies}/>
-            <Route path='/search' component={SearchMovies}/>
-            <Route path='/best' component={BestMovies}/>
-            <Route path='/worst' component={WorstMovies}/>
-        </div>
-    </BrowserRouter>,
-    document.getElementById('root'));
+ReactDOM.render(<Home/>, document.getElementById('root'));
 registerServiceWorker();
