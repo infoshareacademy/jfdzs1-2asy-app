2c6302691aa5a4903791eaa7c506de67c08347c6
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-17 08:23:19 +0100

    Fixed import component

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React, {PureComponent} from 'react';
+import {BrowserRouter, Route} from "react-router-dom";
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import Navigation from './Components/Navigation/Navigation.component';
 import Dashboard from "./Routes/Dashboard/Dashboard.components";
 import SearchMovies from "./Routes/SearchMovies/SearchMovies.component";
@@ -8,12 +10,11 @@ import WorstMovies from "./Routes/WorstMovies/WorstMovies.component";
 import FavouriteMovies from "./Routes/FavoriteMovies/FavoriteMovies.component";
 import BestMovies from "./Routes/BestMovies/BestMovies.components";
 import './Home.css';
-import {BrowserRouter, Route} from "react-router-dom";
 
 class Home extends PureComponent {
     render() {
         return (
-            <MuiThemeProvider muiTheme={getMuiTheme()}>
+            <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
                 <BrowserRouter>
                     <div className="Home">
                         <Navigation/>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
+import './index.css';
 
 ReactDOM.render(<Home/>, document.getElementById('root'));
 registerServiceWorker();
