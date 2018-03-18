24c9f7b0afff38e4053f6dc2cde51d7f1653c9c8
Piotr Leoszewski <p.leoszewski@gmail.com>
2018-03-18 10:54:56 +0100

    Change drawer

--- src/Components/Navigation/Navigation.component.js
+++ src/Components/Navigation/Navigation.component.js
@@ -13,20 +13,20 @@ class Navigation extends PureComponent {
     handleToggle = () => this.setState({open: !this.state.open});
 
     render() {
-        const contentStyle = { transition: 'margin-left 450ms cubic-bezier(0.23, 1, 0.32, 1)' };
-
-        if (this.state.open) {
-            contentStyle.marginLeft = 256;
-        }
 
         return (
             <AppBar
                 title="GitFilm"
-                children={<Drawer open={this.state.open}><Menu/></Drawer>}
-                onLeftIconButtonClick={this.handleToggle}
-                style={contentStyle}
-            />
+                children={<Drawer
 
+                    docked={false}
+                    width={200}
+                    open={this.state.open}
+                    onRequestChange={(open) => this.setState({open})}>
+
+                    <Menu/></Drawer>}
+                onLeftIconButtonClick={this.handleToggle}
+            />
         )
     }
 }
