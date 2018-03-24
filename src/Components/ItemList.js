05669bf489b36036c625edf50ef6ee46b382793f
marcin_ch <bukkosky@gmail.com>
2018-03-24 23:15:40 +0100

    add best and worst movies components, movie list fetch from firebase

---
+++ src/Components/ItemList.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class ItemsList extends Component {
+  render() {
+    const { thumbnail, name, rating } = this.props
+    return (
+      <div>
+        <div>
+          {thumbnail ? <img src={thumbnail} alt="" /> :
+                       <span>NO IMAGE</span>
+          }
+        </div>
+        <div><h4>Tytuł: {name}</h4></div>
+        <div><h4>Rating: {rating}</h4></div>
+      </div>
+    )
+  }
+}
+
+export default ItemsList;
--- src/Routes/BestMovies/BestMovies.components.js
+++ src/Routes/BestMovies/BestMovies.components.js
@@ -1,10 +1,39 @@
 import React, { PureComponent } from 'react';
+import FetchForm from '../../Components/FetchForm';
+import ItemsList from '../../Components/ItemList';
 
+import './BestMovies.style.css';
+
+const url = 'https://gitfilm-675bb.firebaseio.com/asy-app.json'
 class BestMovies extends PureComponent {
+
+    state = {
+        fetchedItems: []
+    }
+
+    fetchData = (fetchItems) => {
+        fetch(url)
+        .then(response => response.json())
+        .then(Object.values)
+        .then(fetchedItems => {
+            this.setState({
+                fetchedItems,
+            })
+        })
+    }
     render() {
+        const { fetchedItems } = this.state
         return(
             <div>
                 <h2>Best Movies</h2>
+                <FetchForm onClick={this.fetchData} />
+                <div>
+				{fetchedItems.map(({thumbnail, name, rating}) => (
+					<ItemsList key={thumbnail} thumbnail={thumbnail}
+							   name={name}
+							   rating={rating} />  
+				))}
+			    </div>
             </div>
         )
     }
